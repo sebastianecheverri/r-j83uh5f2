e787a721f6580484dd0e72e8663cd464d67920a2
Sebastian Echeverri <sebastianecheverri900@gmail.com>
2019-01-15 00:16:59 -0500

    solved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      valueForm: "",
+      numberForm: 0,
+    }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.valueForm} onChange={this.updateValueForm.bind(this)}></textarea>
+        <div className="counter">{this.state.numberForm}</div>
       </div>
     );
   }
+  updateValueForm(event) {
+    
+      this.setState({
+      valueForm: event.target.value,
+      numberForm: event.target.value.length
+    })
+  }
+  
 }
 
 export default App;
